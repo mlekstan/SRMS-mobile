8f4fce24acafc2f208f199140ea6ee809035cb14
Michal Lekstan <m.lekstan@interia.pl>
2025-12-06 21:29:44 +0100

    lang files

---
+++ SRMS-mobile/constants/langs/en.ts
@@ -0,0 +1,78 @@
+export const en = {
+  "login": {
+    "header": "Sign in",
+  },
+
+  "rental": {
+    "header": "Rental",
+    "scannerOn": "Scanner ON",
+    "scannerOff": "Scanner OFF",
+
+    "banner": {
+      "message": "Scanned client card barcode",
+      "button": "Close",
+    },
+  },
+
+  "return": {
+    "header": "Return"
+  },
+
+  "more": {
+    "header": "More",
+    "settings": "Settings",
+    "profile": "My profile",
+    "about": "About app",
+    "signOut": "Sign out",
+  },
+
+  "settings": {
+    "header": "Settings",
+    "theme": {
+      "title": "Theme",
+      "option.light": "Light",
+      "option.dark": "Dark",
+      "option.auto": "System",
+    },
+    "lang": {
+      "title": "Language",
+      "option.pl": "Polish",
+      "option.en": "English",
+    },
+  },
+
+  "profile": {
+    "header": "My profile",
+    "list": {
+      "firstName": "First name",
+      "middleName": "Middle name",
+      "lastName": "Last name",
+      "email": "E-mail",
+      "phoneNumber": "Phone number",
+      "dateJoined": "Date joined",
+    },
+  },
+
+  "about": {
+    "header": "About app",
+  },
+
+  "components": {
+    "form": {
+      "login": {
+        "email.label": "E-mail",
+        "email.error.empty": "Field can't be empty.",
+        "email.error.invalid": "Ivalid e-mail address.",
+        "password.label": "Password",
+        "password.error.empty": "Field can't be empty.",
+        "password.error.tooShort": "Password is too short.",
+        "button": "Sign in",
+      },
+    },
+
+    "errorDialog": {
+      "title": "An error occured",
+      "button": "Close",
+    },
+  }
+};
---
+++ SRMS-mobile/constants/langs/pl.ts
@@ -0,0 +1,81 @@
+import { LangDictType } from "@/types/lang-types";
+
+export const pl: LangDictType = {
+  "login": {
+    "header": "Logowanie",
+  },
+
+  "rental": {
+    "header": "Wypożyczenie",
+    "scannerOn": "Włącz skaner",
+    "scannerOff": "Wyłącz skaner",
+
+    "banner": {
+      "message": "Zeskanowany kod kreskowy karty klienta",
+      "button": "Zamknij",
+    },
+  },
+
+  "return": {
+    "header": "Zwrot"
+  },
+
+  "more": {
+    "header": "Więcej",
+    "settings": "Ustawienia",
+    "profile": "Mój profil",
+    "about": "O aplikacji",
+    "signOut": "Wyloguj",
+  },
+
+  "settings": {
+    "header": "Ustawienia",
+    "theme": {
+      "title": "Motyw",
+      "option.light": "Jasny",
+      "option.dark": "Ciemny",
+      "option.auto": "Systemowy",
+    },
+    "lang": {
+      "title": "Język",
+      "option.pl": "Polski",
+      "option.en": "Angielski",
+    },
+  },
+
+  "profile": {
+    "header": "Mój profil",
+    "list": {
+      "firstName": "Imię",
+      "middleName": "Drugie imię",
+      "lastName": "Nazwisko",
+      "email": "E-mail",
+      "phoneNumber": "Numer telefonu",
+      "dateJoined": "Data dołączenia",
+    },
+  },
+
+  "about": {
+    "header": "O aplikacji",
+  },
+
+
+  "components": {
+    "form": { 
+      "login": {
+        "email.label": "E-mail",
+        "email.error.empty": "Pole nie może być puste.",
+        "email.error.invalid": "Niepoprawny adres e-mail.",
+        "password.label": "Hasło",
+        "password.error.empty": "Pole nie może być puste.",
+        "password.error.tooShort": "Hasło jest zbyt krótkie.",
+        "button": "Zaloguj się ",
+      },
+    },
+
+    "errorDialog": {
+      "title": "Wystapił błąd",
+      "button": "Zamknij",
+    },
+  }
+};
